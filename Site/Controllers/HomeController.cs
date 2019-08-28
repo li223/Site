3bc1eaee9358982765da81ba0ad648685879bbcb
li223 <li223@users.noreply.github.com>
2019-08-28 16:12:04 +0100

    Add project files.

---
+++ Site/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Site.Models;
+
+namespace Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [Route("noscript")]
+        public IActionResult Noscript() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, ErrorCode = this.HttpContext.Response.StatusCode });
+    }
+}
---
+++ Site/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public int ErrorCode { get; set; }
+    }
+}
