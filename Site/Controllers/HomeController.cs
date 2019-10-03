9d2442fe98ff88a35a704d25c81426980ed7e7b1
li223 <li223@users.noreply.github.com>
2019-10-03 18:33:46 +0100

    Add start of shit documentation

--- Site/Controllers/HomeController.cs
+++ Site/Controllers/HomeController.cs
@@ -11,6 +11,12 @@ namespace Site.Controllers
         [Route("noscript")]
         public IActionResult Noscript() => View();
 
+        [Route("docs/chandler")]
+        public IActionResult ChandlerDocs() => View("Views/CHANdler/Docs.cshtml");
+
+        [Route("docs/chandler/objects")]
+        public IActionResult ChandlerObjectDocs() => View("Views/CHANdler/Objects.cshtml");
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, ErrorCode = this.HttpContext.Response.StatusCode });
     }
